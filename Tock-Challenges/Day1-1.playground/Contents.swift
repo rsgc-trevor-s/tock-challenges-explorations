28691dccf2d3be143aa5ebe1be1c4498d8d4f84c
rsgc-trevor-s <student@MacBook-Pro-706-Trevor.local>
2016-01-28 10:29:12 -0500

    Complete First challange

--- Tock-Challenges/Day1-1.playground/Contents.swift
+++ Tock-Challenges/Day1-1.playground/Contents.swift
@@ -2,4 +2,21 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+var str = " "
+
+let month = 2
+let day = 18
+
+let inputMonth = 2
+let inputDay = 18
+
+
+if inputMonth==month && inputDay==day{
+str = "Special"
+}
+else if (inputMonth == month  && inputDay<day || inputMonth < month ) {
+    str = "Before"
+} else
+{
+    str = "After"
+}
